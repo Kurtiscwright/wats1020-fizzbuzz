3fb3fff267c8378a2c33c3ac432b952ed403f525
Kurtis Wright <Kurtiscwright@gmail.com>
2017-03-27 12:10:00 -0700

    fizzBuzz works

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,19 @@
-// Place your FizzBuzz code here. 
+// Place your FizzBuzz code here.
+function fizzBuzz(number) {
+  for (var i = 1; i <= number; i++) {
+    if (i % 3 === 0 && i % 5 === 0){
+    console.log("FizzBuzz");
+    }  else if(i % 3 === 0) {
+    console.log("Fizz");
+    } else if(i % 5 === 0){
+    console.log("Buzz");
+    }  else {
+    console.log(i);
+    }
+  }
+}
+
+var test = 10
+var test2 = 100
+
+fizzBuzz (test);
